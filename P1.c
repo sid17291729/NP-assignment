e18a1b7ca533bcc9471b05a64e8dd245b0e14c9d
unlaxedneurotic <jindalsaransh@gmail.com>
2021-03-24 17:53:18 +0530

    restructured program added input cleaning redirection and pipe modes started

--- P1.c
+++ P1.c
@@ -5,40 +5,73 @@
 #include<stdio.h>
 #include<string.h>
 #include<sys/wait.h>
-#define MAXLEN 100
+#include<ctype.h>
+
+#define ANSI_COLOR_GREEN "\x1b[32m"
+#define ANSI_COLOR_RESET   "\x1b[0m"
+
+#define MAXLEN 1024
+#define MAXARGLEN 100
+enum MODE{PIPE, REDIRECT_OUT, REDIRECT_IN, REDIRECT_APPEND, NONE}
+
+char read_buf[MAXLEN];
+char *arg_list[MAXARGLEN];
+
+int execute(char*input);
+char*get_input();
+
 int main()
 {
-    printf("shell running\n");
-    char buf[MAXLEN];
-    char *arg_list[MAXLEN];
-    int Status;
+    fprintf(stdout,"Shell Running\n");
+    char*input;
     while(1)
     {
-        fputs("prompt>",stdout);
-        fflush(stdout);
-        fgets(buf,MAXLEN,stdin);
-        buf[strlen(buf)-1]='\0';
-        char *c=strtok(buf," ");
-        int arg_ptr=0;
-        
-        while(c)
-        {
-            arg_list[arg_ptr++]=c;
-            c=strtok(NULL," ");
-
-        }
-        arg_list[arg_ptr]=NULL;
+        input = get_input();
+        execute(input);
         printf("\n");
-        pid_t p=fork();
-        if(p==0)
-        {
-           execvp(arg_list[0],arg_list);
-           printf("error\n");
-        }
-        waitpid(p,&Status,0);
-        
-        printf("\nprocess pid=%d, return status=%d\n\n",p,Status);
-        
+    }
+}
+
+int execute(char*input){
+    int Status;
+    pid_t p;
+
+    int arg_ptr=0;
+    char *nex_tok=input;
+    //fix parsing
+    while(nex_tok){
+
+        arg_list[arg_ptr++]=nex_tok;
+        nex_tok=strtok(NULL," ");
 
     }
+    arg_list[arg_ptr]=NULL;
+
+    int mode
+    switch(mode){
+        case NONE:    
+            p=fork();
+            if(p==0){
+                execvp(arg_list[0],arg_list);
+                printf("ERROR\n");
+            }
+            break;
+        
+        default: printf("THIS REDIRECTION IS NOT SUPPORTED YET");
+    }
+    
+    waitpid(p,&Status,0);
+    printf("process pid=%d, return status=%d\n",p,Status);
+    return p;
+}
+
+char*get_input(){
+    fputs(ANSI_COLOR_GREEN"MyShell$ "ANSI_COLOR_RESET,stdout);
+    fflush(stdout);
+    fgets(read_buf,MAXLEN,stdin);
+    int ptr_end = strlen(read_buf)-1;
+    int ptr_strt = 0;
+    while(isspace(read_buf[ptr_end])) read_buf[ptr_end--]='\0';
+    while(isspace(read_buf[ptr_strt])) ptr_strt++;
+    return read_buf+ptr_strt;
 }
